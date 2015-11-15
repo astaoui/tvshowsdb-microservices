3c021b871c0ebcc40a0f64c0535685a959517465
Julien Roy <roy@fullsix.com>
2015-11-15 19:55:53 +0100

    Add Eureka Service Discovery

--- supervisor/src/main/java/com/github/vanroy/tvshowsdb/SupervisorApplication.java
+++ supervisor/src/main/java/com/github/vanroy/tvshowsdb/SupervisorApplication.java
@@ -1,9 +1,13 @@
 package com.github.vanroy.tvshowsdb;
 
+import com.github.vanroy.cloud.dashboard.config.EnableCloudDashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableCloudDashboard
 public class SupervisorApplication {
 
     public static void main(String[] args) {
